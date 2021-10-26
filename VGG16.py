ac0e57fd41bff2ba70e36ec56c56360e3b8fee88
Gaoxiang-Deng <61376742+Gaoxiang-Deng@users.noreply.github.com>
2021-10-26 15:26:51 +1300

    Add files via upload

--- VGG16.py
+++ VGG16.py
@@ -1,250 +1,246 @@
-# %%
-import matplotlib
-import torch
-import torchvision
+# coding: utf-8
 
-from torch import classes
-from torchvision import transforms
+# In[1]:
+# from IPython import get_ipython
+# from keras import Sequential
+# from tensorflow.python.keras.applications.inception_resnet_v2 import InceptionResNetV2
+#
+# get_ipython().run_line_magic('matplotlib', 'inline')
 
-transform = transforms.Compose(
-    [transforms.Resize((224, 224)),
-     transforms.RandomHorizontalFlip(),
-     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.1, hue=0.1),
-     transforms.RandomAffine(degrees=40, translate=None, scale=(1, 2), shear=15, resample=False, fillcolor=0),
-     transforms.ToTensor(),
-     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
-transform_test = transforms.Compose(
-    [transforms.Resize((224, 224)),
-     transforms.RandomHorizontalFlip(),
-     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.1, hue=0.1),
-     transforms.RandomAffine(degrees=40, translate=None, scale=(1, 2), shear=15, resample=False, fillcolor=0),
-     transforms.ToTensor(),
-     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+# In[ ]:
 
-classes = ('cherry', 'strawberry', 'tomato')
+# input tensorflow and keras
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
 
-data_path = "C:/workspace/comp309project/traindata"
-data_all = torchvision.datasets.ImageFolder(root=data_path, transform=transform)
-test_data_path = "C:/workspace/comp309project/testdata"
-test_data = torchvision.datasets.ImageFolder(root=test_data_path, transform=transform_test)
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Activation
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Convolution2D
+from tensorflow.keras import backend as K
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array
+from tensorflow.keras.optimizers import SGD, Adam, RMSprop
+from tensorflow.keras.metrics import categorical_accuracy
+from tensorflow.keras import regularizers
 
-# %%
-import torch.utils.data as data
-from sklearn.model_selection import train_test_split
-from torch.utils.data import SubsetRandomSampler
-
-
-def dataset_sampler(dataset, val_percentage=0.1):
-    """
-    split dataset into train set and val set
-    :param dataset:
-    :param val_percentage: validation percentage
-    :return: split sampler
-    """
-    sample_num = len(dataset)
-    file_idx = list(range(sample_num))
-    train_idx, val_idx = train_test_split(file_idx, test_size=val_percentage, random_state=42)
-    train_sampler = SubsetRandomSampler(train_idx)
-    val_sampler = SubsetRandomSampler(val_idx)
-    return train_sampler, val_sampler
-
-
-train_sampler, val_sampler = dataset_sampler(data_all)
-
-batch_size = 20
-trainloader = data.DataLoader(data_all, batch_size=batch_size, num_workers=0, sampler=train_sampler)
-validloader = data.DataLoader(data_all, batch_size=batch_size, num_workers=0, sampler=val_sampler)
-
-testloader = data.DataLoader(test_data, batch_size=batch_size, num_workers=0)
-##获得一个batch的数据
-for step, (b_x, b_y) in enumerate(trainloader):
-    if step > 0:
-        break
-
-print(b_x.shape)
-print(b_y.shape)
-print("图像的取值范围为：", b_x.min(), "~", b_x.max())
-print(classes)
-# %%
-import matplotlib.pyplot as plt
 import numpy as np
+import random
+import tensorflow as tf
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+import numpy as np
+import random
+
+# Online code
+# from google.colab import drive
+# drive.mount('/content/drive')
 
 
-def imshow(inp, title=None):
-    inp = inp.numpy().transpose((1, 2, 0))
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    inp = std * inp + mean
-    inp = np.clip(inp, 0, 1)
-    plt.imshow(inp)
-    if title is not None:
-        plt.title(title)
-    plt.pause(0.001)  # pause a bit so that plots are updated
+# In[ ]:
 
 
-inputs, classes = next(iter(trainloader))
-# Make a grid from batch
-out = torchvision.utils.make_grid(inputs)
-imshow(out)
+# Set seed
+seed = 1990
+random.seed(seed)
+np.random.seed(seed)
+tf.sets_random_seed(seed)
 
-# %%
-import torchvision.models as models
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model_ft = models.vgg16(pretrained=True)
-
-# %%
-import torch.nn as nn
-import torch.nn.functional as F
+# In[4]:
 
 
-class Net(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+# Set path
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1)  # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+# online
+data_path = 'C:/workspace/traindata/traindata'
+# data_path = "drive/My Drive/Colab Notebooks/small_Train_data"
+
+# local
+# data_path = "Desktop/vu/2018 T2/COMP309/COMP309_2018_Project/Train_data"
+# data_path = "Desktop/vu/2018 T2/COMP309/COMP309_2018_Project/small_Train_data"
 
 
-import torch.optim as optim
+# In[5]:
 
-net = Net()
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-# %%
+# load data
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array
 
-num_epochs = 10
-num_classes = 2
-batch_size = 25
-learning_rate = 0.001
+# build the generator
+# data preprocessing
+train_datagen = ImageDataGenerator(
 
-epochs = 10
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    rescale=1. / 255,
+    validation_split=1 / 4,
+    horizontal_flip=True,
+    fill_mode='nearest',
+    shear_range=0.2,
+    zoom_range=0.2,
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-device
-# %%
-train_losses = []
-valid_losses = []
-for epoch in range(1, num_epochs + 1):
-    train_loss = 0.0
-    valid_loss = 0.0
-    net.train()
-    for data, label in trainloader:
-        data = data.to(device)
-        target = label.to(device)
-        optimizer.zero_grad()
-        # forward-pass: compute-predicted-outputs-by-passing-inputs-to-the-model
-        output = net(data)
-        # calculate-the-batch-loss
-        loss = criterion(output, target)
-        # backward-pass: compute-gradient-of-the-loss-wrt-model-parameters
-        loss.backward()
-        # perform-a-ingle-optimization-step (parameter-update)
-        optimizer.step()
-        # update-training-loss
-        train_loss += loss.item() * data.size(0)
-        # validate-the-model
-        net.eval()
-    for data, target in validloader:
-        data = data.to(device)
-        target = target.to(device)
-        output = net(data)
-        loss = criterion(output, target)
-        # update-average-validation-loss
-        valid_loss += loss.item() * data.size(0)
+)
 
-    # calculate-average-losses
-    train_loss = train_loss / len(trainloader.sampler)
-    valid_loss = valid_loss / len(validloader.sampler)
-    train_losses.append(train_loss)
-    valid_losses.append(valid_loss)
+# normal genrator
+datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    validation_split=1 / 4,
 
-    # print-training/validation-statistics
-    print('Epoch: {} /tTraining Loss: {:.6f} /tValidation Loss: {:.6f}'.format(epoch, train_loss, valid_loss))
+)
 
-print('Finish training')
+# Set batch size
+batch_size = 30
 
-# %%
-PATH = './cifar_net.pth'
-torch.save(net.state_dict(), PATH)
+train_data = train_datagen.flow_from_directory(
+    data_path,
+    target_size=(300, 300),
+    subset='training',
+    batch_size=batch_size
+)
 
-# %%
-# test
-test_dataiter = iter(testloader)
+validation_data = train_datagen.flow_from_directory(
+    data_path,
+    target_size=(300, 300),
+    subset='validation',
+    batch_size=batch_size
+)
 
-# print the image
-image, labels = test_dataiter.next()
-imshow(torchvision.utils.make_grid(image))
-# %%
-net = Net()
-net.load_state_dict(torch.load(PATH))
+train_size = train_data[0][0].shape[0]*len(train_data)
+validation_size = validation_data[0][0].shape[0]*len(validation_data)
+print(validation_size)
 
-# %%
-outputs = net(image)
 
-# %%
-_, predicted = torch.max(outputs, 1)
+# In[6]:
 
-print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
-                              for j in range(4)))
 
-# %%
-correct = 0
-total = 0
-# since we're not training, we don't need to calculate the gradients for our outputs
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        # calculate outputs by running images through the network
-        outputs = net(images)
-        # the class with the highest energy is what we choose as prediction
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+# Set GPU allow growth
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
 
-print('Accuracy of the network on the 10000 test images: %d %%' % (
-        100 * correct / total))
+# In[7]:
 
-# %%
-correct_pred = {classname: 0 for classname in classes}
-total_pred = {classname: 0 for classname in classes}
+from keras import Sequential
+#basic model
+model = Sequential()
+model. add(Flatten(input_shape=(300,300,3)))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.25))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.25))
+model.add(Dense(3, activation='softmax'))
+model.summary()
 
-# again no gradients needed
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        outputs = net(images)
-        _, predictions = torch.max(outputs, 1)
-        # collect the correct predictions for each class
-        for label, prediction in zip(labels, predictions):
-            if label == prediction:
-                correct_pred[classes[label]] += 1
-            total_pred[classes[label]] += 1
 
-# print accuracy for each class
-for classname, correct_count in correct_pred.items():
-    accuracy = 100 * float(correct_count) / total_pred[classname]
-    print("Accuracy for class {:5s} is: {:.1f} %".format(classname, accuracy))
+# In[8]:
 
-# %%
 
-plt.plot(train_losses, label='Training loss')
-plt.plot(valid_losses, label='Validation loss')
-plt.xlabel("Epochs")
-plt.ylabel("Loss")
-plt.legend(frameon=False)
+
+# In[9]:
+
+
+# input keras applications
+from tensorflow.keras.applications import VGG16, InceptionResNetV2
+
+vgg16 = VGG16(include_top=False, input_shape=(300, 300, 3), classes=3)
+ICRNV2 = InceptionResNetV2(include_top=False,input_shape=(300, 300, 3), classes=3)
+
+# Build the model (VCG-16)
+model = Sequential()
+for layer in vgg16.layers:
+    model.add(layer)
+
+# In[10]:
+
+
+# add flatten layer
+model.add(Flatten())
+model.add(Dense(256, activation='relu', kernel_regularizer=regularizers.l2(0.001)))
+model.add(Dropout(0.25))
+model.add(Dense(256, activation='relu', kernel_regularizer=regularizers.l2(0.001)))
+model.add(Dropout(0.25))
+model.add(Dense(3, activation='softmax', activity_regularizer=regularizers.l1(0.001)))
+model.summary()
+
+
+# In[11]:
+
+
+# Set optimizer
+sgd = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
+rmsprop = RMSprop(lr=1e-4, rho=0.9, epsilon=None, decay=0.0)
+adam = Adam(lr=1e-5, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0, amsgrad=True)
+
+# model compile
+model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=[categorical_accuracy])
+
+# In[12]:
+
+
+# Call back
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import EarlyStopping
+
+tbCallBack = TensorBoard(log_dir='log', histogram_freq=0, write_graph=True, write_images=False)
+checkpoint = ModelCheckpoint(filepath='models/best_weights.hdf5', save_best_only='True', monitor='val_loss')
+earlyStop = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='auto')
+
+# In[ ]:
+
+
+# Start training fit the model
+model.fit_generator(
+
+    train_data,
+    steps_per_epoch=len(train_data),
+    verbose=1,
+    epochs=50,
+    validation_data=validation_data,
+    validation_steps=len(validation_data),
+    callbacks=[tbCallBack, checkpoint])  # <-- add earlyStop to get the best model from call back
+
+# In[ ]:
+
+
+# Evaluate
+model.load_weights('models/best_weights.hdf5')
+loss_and_metrics = model.evaluate_generator(validation_data, len(validation_data))
+print('Test loss:{}\nTest accuracy:{}'.format(loss_and_metrics[0], loss_and_metrics[1]))
+
+# In[ ]:
+
+
+# save model
+import shutil
+
+model.call
+model.save('models/VGG16_sgd_prepor_FULL_loss 0.5362')
+
+# In[ ]:
+
+
+# Test model
+data_path = "C:/workspace/traindata/testdata"
+model_path = 'models/best_weights.hdf5'
+
+test_datagen = ImageDataGenerator(
+
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    rescale=1. / 255,
+    horizontal_flip=True,
+    fill_mode='nearest',
+    shear_range=0.2,
+    zoom_range=0.2
+
+)
+
+test_data = test_datagen.flow_from_directory(
+    data_path,
+    target_size=(300, 300),
+    batch_size=30
+)
+
+loss_and_metrics = model.evaluate_generator(test_data, steps=len(test_data))
+print('Test loss:{}\nTest accuracy:{}'.format(loss_and_metrics[0], loss_and_metrics[1]))
